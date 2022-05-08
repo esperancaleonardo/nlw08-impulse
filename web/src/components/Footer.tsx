f4c986a7ccc6a26b43f6003a3968485fc88bd8e4
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-05-08 03:09:04 -0300

    link href footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -2,7 +2,13 @@ export function Footer() {
   return (
     <footer className="text-xs text-white">
       Feito na base do ☕ por{" "}
-      <a className="underline underline-offset-2">Leonardo Esperança</a>
+      <a
+        className="underline underline-offset-2"
+        href="https://www.linkedin.com/in/leonardo-esperanca/"
+      >
+        Leonardo Esperança
+      </a>
     </footer>
   );
 }
+("");
