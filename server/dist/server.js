b22ab1158cecf089f6769295a36abdf45552cd52
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-05-08 02:09:30 -0300

    ajustes backend deploy

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,5 +1,18 @@
 "use strict";
-const test = 1;
-async function printOk() {
-    return 'OK';
-}
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const routes_1 = require("./routes");
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)({
+    origin: 'http://localhost:3000'
+}));
+app.use(express_1.default.json({ limit: '50mb' }));
+app.use(express_1.default.urlencoded({ limit: '50mb' }));
+app.use(routes_1.routes);
+app.listen(3333, () => {
+    console.log("Http server running!");
+});
