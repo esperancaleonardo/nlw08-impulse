dddaf7d85f45d063a0080a3406fac830fac302d4
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-05-08 02:44:41 -0300

    alterando cor footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="text-xs text-black">
+    <footer className="text-xs text-white">
       Feito na base do ☕ por{" "}
       <a className="underline underline-offset-2">Leonardo Esperança</a>
     </footer>
