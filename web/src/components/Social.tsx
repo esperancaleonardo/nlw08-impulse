c1b6693c7fbfe2d4eee6c92c780810b9db39ab0e
leonardo esperança <leonardo.oliveira.esperanca@gmail.com>
2022-05-08 03:04:08 -0300

    ajustes finais ao projeto

--- web/src/components/Social.tsx
+++ web/src/components/Social.tsx
@@ -4,12 +4,12 @@ import { WidgetSocial } from "./WidgetSocial";
 
 export function Social() {
   return (
-    <Popover className="absolute bottom-2 left-2">
+    <Popover className="absolute bottom-2 left-2 md:bottom-7 md:left-7">
       <Popover.Panel>
         <WidgetSocial />
       </Popover.Panel>
 
-      <Popover.Button className="bg-green-700 rounded-full px-4 h-12 flex items-center text-white group">
+      <Popover.Button className="bg-green-700 rounded-full px-3 h-12 flex items-center text-white group">
         <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-200 ease-linear">
           Redes Sociais <span className="pl-2"></span>
         </span>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       },
       borderRadius: {
         md: '4px'
-      }
+      },
     },
   },
   plugins: [
